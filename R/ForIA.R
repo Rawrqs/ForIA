439c3bd51c4cf95cfe06ad17d12f3970cb0126bb
Rawrqs <commiq@gmail.com>
2014-10-03 13:32:03 +0200

    update on variables
    fixed variable's names and added better print method

--- R/ForIA.R
+++ R/ForIA.R
@@ -1,19 +1,19 @@
 #'setData function
 
-setData <- function(data,  positive    = rep(NA, length(colnames(dane)[-1])), 
-                           negative    = rep(NA, length(colnames(dane)[-1])), 
-                           must.have   = rep(NA, length(colnames(dane)[-1])), 
-                           at.most     = rep(NA, length(colnames(dane)[-1])))
+setData <- function(data,  positive    = rep(NA, length(colnames(data)[-1])), 
+                           negative    = rep(NA, length(colnames(data)[-1])), 
+                           must.have   = rep(NA, length(colnames(data)[-1])), 
+                           at.most     = rep(NA, length(colnames(data)[-1])))
 {
    positive    <- positive==1
    negative    <- negative==1
    must.have   <- must.have == 1
    at.most  <- at.most == 1
    
-   names(positive)   <- colnames(dane)[-1]
-   names(negative)   <- colnames(dane)[-1]
-   names(must.have)  <- colnames(dane)[-1]
-   names(at.most)    <- colnames(dane)[-1]
+   names(positive)   <- colnames(data)[-1]
+   names(negative)   <- colnames(data)[-1]
+   names(must.have)  <- colnames(data)[-1]
+   names(at.most)    <- colnames(data)[-1]
    
    return(list(positive    = positive,
                negative    = negative,
@@ -44,7 +44,7 @@ setSpec <- function(data,
 #'getModels function
 #'Moar
 
-getModels <- function(data, specs = setSpec(data))   
+getModels <- function(data, specs = setSpec(data), signs = setData(data))   
 {
 
 lst   <-list()
@@ -56,26 +56,26 @@ R2.DW <- vector()
 n.max          <- 0
 progress.bar   <-0
 for   (i in 1:specs$nv.max) {
-      for   (s in 1:dim(combn(length(colnames(dane)[-1]),i))[2]){
+      for   (s in 1:dim(combn(length(colnames(data)[-1]),i))[2]){
             n.max <- n.max + 1
    }}
 
 
 pb <- txtProgressBar(min = 0, max = n.max , style = 3)
 for   (i in 1:specs$nv.max) {
-      for (s in 1:dim(combn(length(colnames(dane)[-1]),i))[2]){
-      #print(combn(length(colnames(dane)[-1]),i)[,s])
+      for (s in 1:dim(combn(length(colnames(data)[-1]),i))[2]){
+      #print(combn(length(colnames(data)[-1]),i)[,s])
       
       progress.bar <- progress.bar + 1
       Sys.sleep(0.1)
       # update progress bar
       setTxtProgressBar(pb, progress.bar)  
       
-      kolumny<-combn(length(colnames(dane)[-1]),i)[,s]
-      #if (dim(dane[,c(1,kolumny+1)])[1] < 4) next
+      kolumny<-combn(length(colnames(data)[-1]),i)[,s]
+      #if (dim(data[,c(1,kolumny+1)])[1] < 4) next
       
-      b <-lm(dane[,c(1,kolumny+1)])
-      #fit b <- glm(dane[,c(1,kolumny+1)],family=gaussian())
+      b <-lm(data[,c(1,kolumny+1)])
+      #fit b <- glm(data[,c(1,kolumny+1)],family=gaussian())
        
       lst   <- c(lst,list(b))
       R2    <- c(R2,summary(b)$adj.r.squared)
@@ -94,29 +94,30 @@ return(list(models   = lst,
             R2       = R2,
             R2.DW    = R2.DW,
             data     = data,
-            specs    = specs))
+            specs    = specs,
+            signs    = signs))
 }
 
 #'restrykcjeModele function
 
-restrykcjeModele <- function(object, specs = object$specs) {
+restrykcjeModele <- function(object, specs = object$specs, signs = setData(object$data)) {
    temp <- numeric()
    
    for (i in 1:length(object$models)) {
-      if(any(c(  !negative[c(names(which(object$models[[i]]$coefficients[-1]>=0)))] ,  !positive[c(names(which(object$models[[i]]$coefficients[-1]<=0)))],          summary(object$models[[i]])[[4]][,4][-1]<alfa)==FALSE,na.rm=TRUE)) next # warunki na znaki oraz istotnos
+      if(any(c(  !signs$negative[c(names(which(object$models[[i]]$coefficients[-1]>=0)))] ,  !signs$positive[c(names(which(object$models[[i]]$coefficients[-1]<=0)))],          summary(object$models[[i]])[[4]][,4][-1]<specs$alfa)==FALSE,na.rm=TRUE)) next # warunki na znaki oraz istotnos
 
-      if(summary(object$models[[i]])$r.squared<R.s) next # r kwadrat adjusted 
+      if(summary(object$models[[i]])$r.squared<specs$R.s) next # r kwadrat adjusted 
       
-      if(jarque.bera.test(residuals(object$models[[i]]))[3]$p.value<j.b) next #normalnosc 
+      if(jarque.bera.test(residuals(object$models[[i]]))[3]$p.value<specs$j.b) next #normalnosc 
       
-      if(bptest(object$models[[i]])[4]$p.value<b.p) next # heteroskedastycznosc
+      if(bptest(object$models[[i]])[4]$p.value<specs$b.p) next # heteroskedastycznosc
       
-      if(any(must.have, na.rm=TRUE)==TRUE){
-         if(!any(must.have[c(names(object$models[[i]]$coefficients[-1]))],na.rm=TRUE)==TRUE) next #must have
+      if(any(signs$must.have, na.rm=TRUE)==TRUE){
+         if(!any(signs$must.have[c(names(object$models[[i]]$coefficients[-1]))],na.rm=TRUE)==TRUE) next #must have
       }
       
-      if(any(at.most, na.rm=TRUE)==TRUE){
-         if(sum(at.most[c(names(object$models[[i]]$coefficients[-1]))],na.rm=TRUE) > m) next #at.most
+      if(any(signs$at.most, na.rm=TRUE)==TRUE){
+         if(sum(signs$at.most[c(names(object$models[[i]]$coefficients[-1]))],na.rm=TRUE) > specs$m) next #at.most
       }
       
       temp <- c(temp, i)
@@ -127,7 +128,8 @@ restrykcjeModele <- function(object, specs = object$specs) {
                 R2      = object$R2[temp],
                 R2.DW   = object$R2.DW[temp],
                 data    = object$data,
-                specs   = object$specs))
+                specs   = object$specs,
+                signs   = object$signs))
                 
 }
 
@@ -143,7 +145,16 @@ topModels <- function(object, a = 9, restrictions = TRUE, ...)
    
    #names(R2temp) <- 1:length(lista)
    for(i in 1:a){
-      print(summary(object$models[[as.numeric(names(sort(object$R2.DW, decreasing = TRUE)))[i]]]))
+      #print(summary(object$models[[as.numeric(names(sort(object$R2.DW, decreasing = TRUE)))[i]]]))
+      
+      print(cat(cat(colnames(object$data)[as.numeric(names(sort(object$R2.DW, decreasing = TRUE)))[i]], " = "), cat(names(object$models[[as.numeric(names(sort(object$R2.DW, decreasing = TRUE)))[i]]]$coefficients), sep = " + ")))
+      cat("\n")
+      print(summary(object$models[[as.numeric(names(sort(object$R2.DW, decreasing = TRUE)))[i]]])[[4]])
+      cat("\n")
+      cat(paste0("R2: ", round(object$R2[[as.numeric(names(sort(object$R2.DW, decreasing = TRUE)))[i]]],2), ", R2 DW: ", round(object$R2.DW[[as.numeric(names(sort(object$R2.DW, decreasing = TRUE)))[i]]],2)))
+      cat("\n")
+      cat("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+      cat("\n")
       
       if(is.null(object$data) == FALSE) {
          plot(predict(object$models[[as.numeric(names(sort(object$R2.DW, decreasing = TRUE)))[i]]], object$data), col = "red", type = "l", xlab = "", ylab = "")
@@ -153,13 +164,3 @@ topModels <- function(object, a = 9, restrictions = TRUE, ...)
    }
    par(mfrow=c(1,1))
 }
-
-#'saveModels function
-
-saveModels <- function(object, ...) saveRDS(object, ...)
-
-#'loadModels function
-
-loadModels <- function(...) load(...)
-
-
